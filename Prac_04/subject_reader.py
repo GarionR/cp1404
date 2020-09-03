dcb40b371b4fe05d9c554e33ed293424e9790b47
g22ar <garion.rains@my.jcu.edu.au>
2020-09-03 22:57:53 +1000

    added data variable to get_data function added print_data function

--- Prac_04/subject_reader.py
+++ Prac_04/subject_reader.py
@@ -8,11 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    # print(data)
+    print_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +25,19 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_data(data):
+    longest_name = ""
+    for subject in data:
+        if len(subject[1]) > len(longest_name):
+            longest_name = subject[1]
+
+    for subject in data:
+        print("{} is taught by {:<{}} and has {:>3} students".format(subject[0], subject[1], len(longest_name), subject[2]))
 
 
 main()
