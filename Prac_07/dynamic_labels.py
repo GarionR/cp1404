ceea0f3ebd9b7c6da4bc46b596512580a00ffd82
g22ar <garion.rains@my.jcu.edu.au>
2020-10-06 13:08:54 +1000

    add dynamic_labels.kv add dynamic_labels.py

---
+++ Prac_07/dynamic_labels.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Me", "My friend", "My other friends"]
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.entries_box.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
