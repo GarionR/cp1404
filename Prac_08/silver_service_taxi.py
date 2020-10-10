1d8efef8955011e644772ff5048c54205539cbfe
g22ar <garion.rains@my.jcu.edu.au>
2020-10-10 14:27:15 +1000

    update silver_service_taxi_test.py, silver_service_taxi.py, taxi.py to round fares to the nearest 10c

--- Prac_08/silver_service_taxi.py
+++ Prac_08/silver_service_taxi.py
@@ -12,7 +12,7 @@ class SilverServiceTaxi(Taxi):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance + self.flagfall
+        return super().get_fare() + self.flagfall
 
     def __str__(self):
         """Return a string like a Taxi but with flag_fall."""
