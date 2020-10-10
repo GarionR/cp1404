f00bd7a2893d1a861a523097deb12b4457def95d
g22ar <garion.rains@my.jcu.edu.au>
2020-10-10 14:20:46 +1000

    add silver_service_taxi_test.py

---
+++ Prac_08/silver_service_taxi_test.py
@@ -0,0 +1,7 @@
+from Prac_08.silver_service_taxi import SilverServiceTaxi
+
+taxi_1 = SilverServiceTaxi("Hummer", 200, 4)
+print(taxi_1)
+taxi_2 = SilverServiceTaxi("My Taxi", 1000, 2)
+print(taxi_2.drive(18))
+print("Fare = ${}".format(taxi_2.get_fare()))
