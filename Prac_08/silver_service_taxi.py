e7f182cf61bda757b3b99e3fca0ce52d38d9d370
g22ar <garion.rains@my.jcu.edu.au>
2020-10-10 14:11:48 +1000

    add silver_service_taxi.py

---
+++ Prac_08/silver_service_taxi.py
@@ -0,0 +1,19 @@
+from Prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Txi that scales price_per_km by fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name="", fuel=0, fanciness=1):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance + self.flagfall
+
+    def __str__(self):
+        """Return a string like a Taxi but with flag_fall."""
+        return "{} plus flagfall of ${}".format(super().__str__(), self.flagfall)
